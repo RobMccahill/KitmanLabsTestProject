8f579b56cbf0871d748eb8d2c78d4cc56a900ace
Robert Mccahill <robert.mccahil@gmail.com>
2025-05-14 20:52:44 +0100

    Initial Commit

---
+++ KitmanLabsTestProject/KitmanLabsTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitmanLabsTestProjectApp.swift
+//  KitmanLabsTestProject
+//
+//  Created by Robert Mccahill on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct KitmanLabsTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KitmanLabsTestProjectTests/KitmanLabsTestProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  KitmanLabsTestProjectTests.swift
+//  KitmanLabsTestProjectTests
+//
+//  Created by Robert Mccahill on 14/05/2025.
+//
+
+import Testing
+
+struct KitmanLabsTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
